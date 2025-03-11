e27da176e293cdeabaa3169696d8818f3fccdcf6
Kanish23 <115355754+Kanish23@users.noreply.github.com>
2025-03-11 17:27:30 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Function to navigate to a new page
+function navigateTo(page) {
+    window.location.href = page;
+}
+
+// Function to go back to the previous page
+function goBack() {
+    window.history.back();
+}
