b406e0ef0148994fb4c8d47c480ed5e2ab66ec8d
Kanish23 <115355754+Kanish23@users.noreply.github.com>
2025-03-25 10:15:46 -0400

    Update login.js

--- login.js
+++ login.js
@@ -1,5 +1,18 @@
-import { auth } from "./firebase.js";
-import { signInWithEmailAndPassword } from "firebase/auth";
+const firebaseConfig = {
+  apiKey: "AIzaSyDdsM6Y0XSIuB24w5P0wQT5JvTxkr4QV2Y",
+  authDomain: "astronish-bd18d.firebaseapp.com",
+  projectId: "astronish-bd18d",
+  storageBucket: "astronish-bd18d.firebasestorage.app",
+  messagingSenderId: "232257360387",
+  appId: "1:232257360387:web:fd9f82a09f585df6a33436",
+  measurementId: "G-X41L76SELD"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Sign-up logic
+const auth = firebase.auth();
 
 // Login Function
 document.getElementById("login-form").addEventListener("submit", function (e) {
