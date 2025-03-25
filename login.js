034a80b16cef0a3bffb9bcfbadd77b708fef0543
Kanish23 <115355754+Kanish23@users.noreply.github.com>
2025-03-25 10:27:01 -0400

    Update login.js

--- login.js
+++ login.js
@@ -11,8 +11,6 @@ const firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-// Sign-up logic
-const auth = firebase.auth();
 
 // Login Function
 document.getElementById("login-form").addEventListener("submit", function (e) {
@@ -21,7 +19,7 @@ document.getElementById("login-form").addEventListener("submit", function (e) {
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
 
-    signInWithEmailAndPassword(auth, email, password)
+     firebase.auth().signInWithEmailAndPassword(email, password)
         .then((userCredential) => {
             alert("Login successful! Redirecting...");
             window.location.href = "index.html"; // Redirect to home page
