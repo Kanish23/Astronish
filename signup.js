246a1e1d6466562e264ed463130fc77c9ecc6125
Kanish23 <115355754+Kanish23@users.noreply.github.com>
2025-03-25 10:15:30 -0400

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,5 +1,18 @@
-import { auth } from "./firebase.js";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+const firebaseConfig = {
+  apiKey: "AIzaSyDdsM6Y0XSIuB24w5P0wQT5JvTxkr4QV2Y",
+  authDomain: "astronish-bd18d.firebaseapp.com",
+  projectId: "astronish-bd18d",
+  storageBucket: "astronish-bd18d.firebasestorage.app",
+  messagingSenderId: "232257360387",
+  appId: "1:232257360387:web:fd9f82a09f585df6a33436",
+  measurementId: "G-X41L76SELD"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Sign-up logic
+const auth = firebase.auth();
 
 // Sign Up Function
 document.getElementById("signup-form").addEventListener("submit", function (e) {
