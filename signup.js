0d995ab2b4427453fa6eafb7ff05643f8849e92a
Kanish23 <115355754+Kanish23@users.noreply.github.com>
2025-03-26 11:27:46 -0400

    Update signup.js

--- signup.js
+++ signup.js
@@ -20,7 +20,7 @@ document.getElementById("signup-form").addEventListener("submit", function (e) {
 
     firebase.auth().createUserWithEmailAndPassword(email, password)
         .then((userCredential) => {
-            alert("Sign-up successful! Redirecting...");
+            // alert("Sign-up successful! Redirecting...");
             window.location.href = "index.html"; // Redirect to home page
         })
         .catch((error) => {
