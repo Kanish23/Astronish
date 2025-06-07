dfc79e8aeec94ffc305dead2bfd83f80d028136a
Kanish23 <115355754+Kanish23@users.noreply.github.com>
2025-06-06 21:44:06 -0400

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDdsM6Y0XSIuB24w5P0wQT5JvTxkr4QV2Y",
+  authDomain: "astronish-bd18d.firebaseapp.com",
+  projectId: "astronish-bd18d",
+  storageBucket: "astronish-bd18d.firebasestorage.app",
+  messagingSenderId: "232257360387",
+  appId: "1:232257360387:web:fd9f82a09f585df6a33436",
+  measurementId: "G-X41L76SELD"
+};
+
+firebase.initializeApp(firebaseConfig);
